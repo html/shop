cbd8672d0d481d7003603f597202b33661378c3b
Olexiy Zamkoviy <olexiy.z@gmail.com>
2011-04-05 11:20:21 +0300

    Fixed bug for deleting image for product

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def product_price(product)
     product.master.price.to_s + " грн."
   end
+
+  def display_product_image_for_list(product)
+    image_tag product.images.first.attachment.url(:product_list)
+  end
 end
